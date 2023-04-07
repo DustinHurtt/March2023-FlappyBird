3b325a262d1510801e5126928c142a9aaef36d34
Dustin Hurtt <dustin_hurtt@outlook.com>
2023-04-07 10:39:42 -0400

    canvas appears

--- js/script.js
+++ js/script.js
@@ -1,10 +1,33 @@
+
+
+
+
+
+
+
+
+function startGame() {
+  // let logo = document.getElementById('logo')
+  // logo.style.visibility = 'hidden'
+  // logo.style.height = '0px'
+  console.log('starting')
+}
+
+
 window.onload = function() {
   document.getElementById("start-button").onclick = function() {
+    let logo = document.getElementById('logo')
+    logo.style.visibility = 'hidden'
+    logo.style.height = '0px'
+
+    let container = document.getElementById('game-board')
+    container.style.visibility = 'visible'
+    container.style.height = '600px'
+
+    let gameBoard = document.getElementById('my-canvas')
+    gameBoard.height = '600'
+    gameBoard.width = '1200'
+
     startGame();
   };
-
-  function startGame() {
-
-  }
-
 };
