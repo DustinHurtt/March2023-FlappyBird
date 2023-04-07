3c1f3081a347b1e85bea1ee1124aa430157f3042
Dustin Hurtt <dustin_hurtt@outlook.com>
2023-04-07 10:51:20 -0400

    added background

--- js/script.js
+++ js/script.js
@@ -1,33 +1,56 @@
+const canvas = document.getElementById('my-canvas')
+const ctx = canvas.getContext('2d')
+
+let gameOn = false
+
+let animationId
+
+const background = new Image()
+background.src = './images/bg.png'
 
 
 
 
 
+function animationLoop() {
 
+  ctx.clearRect(0, 0, 1200, 600)
 
+  ctx.drawImage(background, 0, 0, 1200, 600)
+
+  
+
+}
 
 function startGame() {
-  // let logo = document.getElementById('logo')
-  // logo.style.visibility = 'hidden'
-  // logo.style.height = '0px'
+
+  gameOn = true
+
+  animationId = setInterval(animationLoop, 16)
+
   console.log('starting')
 }
 
 
 window.onload = function() {
   document.getElementById("start-button").onclick = function() {
-    let logo = document.getElementById('logo')
-    logo.style.visibility = 'hidden'
-    logo.style.height = '0px'
 
-    let container = document.getElementById('game-board')
-    container.style.visibility = 'visible'
-    container.style.height = '600px'
+    if (!gameOn) {
+      
+      let logo = document.getElementById('logo')
+      logo.style.visibility = 'hidden'
+      logo.style.height = '0px'
+  
+      let container = document.getElementById('game-board')
+      container.style.visibility = 'visible'
+      container.style.height = '600px'
+  
+      let gameBoard = document.getElementById('my-canvas')
+      gameBoard.height = '600'
+      gameBoard.width = '1200'
+  
+      startGame();
 
-    let gameBoard = document.getElementById('my-canvas')
-    gameBoard.height = '600'
-    gameBoard.width = '1200'
-
-    startGame();
+    }
   };
 };
